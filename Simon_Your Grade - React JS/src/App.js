7ef2fb1d7783fb8ec5c47c24ca830fb26deed96d
Andrei Simon <111426784+avflsimon@users.noreply.github.com>
2023-11-22 10:48:57 +0800

    Add files via upload

---
+++ Simon_Your Grade - React JS/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Form2Table from './components/Form2Table'
+
+function App() {
+  return (
+    <div className='App'>
+      <Form2Table />
+    </div>
+  )
+}
+
+export default App
